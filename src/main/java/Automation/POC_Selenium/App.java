f6dc9ea91ef8982f667dd2c51e71cfabe6d671cd
Sumit <snnarangsumit61@gmail.com>
2024-06-16 05:16:09 +0530

    Initial Commit

---
+++ src/main/java/Automation/POC_Selenium/App.java
@@ -0,0 +1,13 @@
+package Automation.POC_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
